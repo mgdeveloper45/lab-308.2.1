51ccca01139b7c1bef69ae7f54e3804cdd08b9e1
MarkGee45 <moel55.mg@gmail.com>
2024-10-25 08:07:36 -0700

    track plant count and week and calculate growth for each week

--- script.js
+++ script.js
@@ -9,4 +9,10 @@ const maxCapacity = Math.floor(area / spacePerPlant);
 // console.log(maxCapacity)
 const fiftyPercentCapacity = maxCapacity * 0.5;
 const eightyPercentCapacity = maxCapacity * 0.8;
-console.log(fiftyPercentCapacity, eightyPercentCapacity)
+// console.log(fiftyPercentCapacity, eightyPercentCapacity)
+let plantCount = initPlantCount;
+
+for(let i = 1; i <= 3; i++) {
+    plantCount *= 2;
+    console.log(`week: ${i}, plant count: ${plantCount}`);
+}
