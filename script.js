bbcd723294eedd72f1b8ec658ab18a3b1da0aa0f
MarkGee45 <moel55.mg@gmail.com>
2024-10-25 07:34:35 -0700

    calculated the maximum capacity of plants that the garden can hold

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 const PI = 3.1415;
 const radius = 5;
 const area = PI * radius * radius;
-console.log(area)
+// console.log(area)
+const spacePerPlant = 0.8;
+const initPlantCount = 20;
+const maxCapacity = area / spacePerPlant;
+// console.log(maxCapacity)
+
